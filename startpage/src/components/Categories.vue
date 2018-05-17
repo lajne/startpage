e3dd5571f26a695ff6d620dbd7bffd10d006c43d
malloryfraiche <malloryfraiche@gmail.com>
2018-05-17 14:56:08 +0200

    updates in categories.vue

--- startpage/src/components/Categories.vue
+++ startpage/src/components/Categories.vue
@@ -1,18 +1,19 @@
 <template>
+
   <div class="container-fluid">
+    
     <div class="card-deck">
+      
       <div class="card">
         <div class="card-body">
-
-
-          <div class="card-title">{{ title }}</div>
+          <div class="card-title">CATEGORY 1</div>
           <div class="row">
             <div class="col">
               <div class="card-deck">
                 <div class="card">
                   <div class="card-body">
                     <div class="card-image">
-                      <i></i>
+                      <i class="fa fa-search prefix"></i>
                     </div>
                     <div class="card-text"></div>
                   </div>
@@ -28,19 +29,49 @@
               </div>
             </div>
           </div>
-
-         
+          <!-- long card div -->
+          <!-- <div class="card-deck"> -->
+            <div class="card mb-3">
+              <div class="card-body">
+                <div class="card-image">
+                  <i class="fa fa-search prefix"></i>
+                </div>
+                <div class="card-text"></div>
+              </div>
+            </div>
+          <!-- </div> -->
         </div> 
       </div>
     </div>
-  </div>
+    
+    
+    <div class="card">
+      <div class="card-body">
+        <div class="card-title">CATEGORY 2</div>
+        <div class="card">
+          <div class="card-body">
+            <!-- HR link and icon here -->
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-body">
+            <!-- IT link and icon here -->
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    
+  </div><!-- end card-deck -->
   
+</div><!-- end container-fluid div -->
 
-  
+
 </template>
 
 
 <script>
+
 export default {
     name: 'Categories',
     props: {
