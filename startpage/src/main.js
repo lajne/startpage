64fba45ab2326c8cbbf5b5fd5375a0747832b168
laban211 <lalaban211@gmail.com>
2018-05-22 14:15:56 +0200

    Smal changes, nothing important

--- startpage/src/main.js
+++ startpage/src/main.js
@@ -3,9 +3,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
 import MontiorIcon from "vue-material-design-icons/monitor.vue"
+import WebIcon from "vue-material-design-icons/web.vue"
 
 Vue.use(BootstrapVue);
 Vue.component("monitor-icon", MontiorIcon);
+Vue.component("web-icon", WebIcon);
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'mdbootstrap/css/mdb.css';
